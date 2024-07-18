5cbc6a4a53ea3e567002da81f2c479c59bd96602
Hanzla50 <hanzlajaved50281@gmail.com>
2024-07-18 09:00:13 +0500

    complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,13 @@
 import 'package:flutter/material.dart';
-
-//import 'package:flutter/material.dart';
+import 'package:practice/gradient_container.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [Color.fromARGB(255, 195, 221, 242), Colors.blue],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const Center(
-            child: Text('First App', style: TextStyle(color: Colors.white , fontSize: 28,),),),
-          ),
-        ),
+        body: GradientContainer(),
       ),
+    ),
   );
 }
+
